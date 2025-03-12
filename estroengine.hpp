a0943b319e54b7f2ce8e488b1cad1330b59a1452
RobinsAviary <robinsaviary@gmail.com>
2025-03-12 17:24:06 -0400

    Got children/garbage collection system working, added onDestroy()

--- estroengine.hpp
+++ estroengine.hpp
@@ -26,39 +26,152 @@ class rNode {
 	public:
 		virtual void step() {}
 		virtual void draw() {}
+		virtual void onDestroy() {}
+
+		rNode* parent = nullptr;
 
 		void childrenStep() {
-			int childrenSize = children.size();
-			for (int i = 0; i < + )
+			if (children.size() != 0) {
+				for (auto child : children) {
+					child->step();
+					child->childrenStep();
+				}
+			}
 		}
 
 		void childrenDraw() {
-
+			if (children.size() != 0) {
+				for (auto child : children) {
+					child->draw();
+					child->childrenDraw();
+				}
+			}
 		}
 
-		std::unique_ptr<rNode>* addNode(rNode* pointer) {
+		template <typename T>
+		T* addNode() {
+			rNode* pointer = new T;
 			if (pointer) {
-				children.push_back(std::unique_ptr<rNode>(pointer));
+				children.push_back(pointer);
 			}
+
+			pointer->parent = this;
 			
-			return &children[children.size() - 1];
+			return static_cast<T*>(pointer);
 		}
 
 		void removeNode(int index) {
 			children.erase(children.begin() + index);
 		}
 
-		std::unique_ptr<rNode>* insertNode(int index, rNode* pointer) {
-			if (pointer) {
-				children.insert(children.begin() + index, std::unique_ptr<rNode>(pointer));
+		void destroy() {
+			onDestroy();
+
+ 			auto children = getAllChildren();
+
+			for (auto child : children) {
+				delete child;
+				child = nullptr;
+
+				int i = 0;
 			}
 
-			return &children[index];
+			if (parent != nullptr) {
+				int parentChildrenSize = parent->children.size();
+				for (int i = 0; i < parentChildrenSize; i++) {
+					if (parent->children[i] == this) {
+						parent->children[i] = nullptr;
+						parent->children.erase(parent->children.begin() + i);
+						break;
+					}
+				}
+			}
+
+			delete this;
+		}
+
+		template <typename T>
+		T* insertNode(int index) {
+			rNode* pointer = new T;
+
+			if (pointer) {
+				children.insert(children.begin() + index, pointer);
+			}
+
+			pointer->parent = this;
+
+			return static_cast<T*>(children[index]);
+		}
+
+		std::vector<rNode*> getChildren() {
+			return children;
+		}
+
+		std::vector<rNode*> getAllChildren() {
+			std::vector<rNode*> result;
+
+			// Recursively iterate through hierarchy.
+			std::vector<rNode*> unexploredNodes;
+
+			unexploredNodes = children;
+			result = children;
+
+			while (unexploredNodes.size() > 0) {
+				rNode* node = unexploredNodes.back(); // Get last node
+				unexploredNodes.pop_back(); // Remove last node
+				
+				if (node->children.size() != 0) { // If current node has children
+					for (auto child : node->children) {
+						unexploredNodes.push_back(child); // Push back all children to the unexplored nodes and result.
+						result.push_back(child);
+					}
+				}
+			}
+
+			return result;
+		}
+
+		bool hasTag(std::string tag) {
+			return (std::find(tags.begin(), tags.end(), tag) != tags.end());
+		}
+
+		std::vector<rNode*> getChildrenTagged(std::string tag) {
+			std::vector<rNode*> result;
+			
+			for (auto child : children) {
+				if (child->hasTag(tag)) {
+					result.push_back(child);
+				}
+			}
+
+			return result;
+		}
+
+		// TODO: std::vector<rNode*> getAllChildrenTagged(std::string tag)
+
+		void destroyChildrenTagged(std::string tag) {
+			auto childrenCopy = children;
+
+			for (auto child : childrenCopy) {
+				if (child->hasTag(tag)) {
+					child->destroy();
+				}
+			}
+		}
+
+		void addTag(std::string tag) {
+			if (!hasTag(tag)) {
+				tags.push_back(tag);
+			}
+		}
+
+		void removeTag(std::string tag) {
+			tags.erase(std::remove(tags.begin(), tags.end(), tag), tags.end());
 		}
 
 	private:
 		std::vector<std::string> tags;
-		std::vector<std::unique_ptr<rNode>> children;
+		std::vector<rNode*> children;
 };
 
 class rNode2D : public rNode {
@@ -94,9 +207,15 @@ public:
 private:
 	void step() {
 		root->step();
+		root->childrenStep();
 	}
 
 	void draw() {
+		rBeginDraw();
+		
 		root->draw();
+		root->childrenDraw();
+
+		rEndDraw();
 	}
 };
---
+++ include/estroextras.hpp
@@ -0,0 +1,325 @@
+// Estroextras!
+#pragma once
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+#include "estrocolors.hpp"
+#include <functional>
+#include <vector>
+
+class rColorBackground : public rNode {
+	public:
+		rColor color = cBlack;
+
+		void draw() {
+			rDrawClear(color);
+		}
+};
+
+float deg2rad(float deg) {
+    return deg * (M_PI / 180);
+}
+
+float lengthdir_x(float length, float dir) {
+    return (sin(deg2rad(dir)) * length);
+}
+
+float lengthdir_y(float length, float dir) {
+    return (tan(deg2rad(dir)) * length);
+}
+
+float lengthdir_z(float length, float dir) {
+    return (cos(deg2rad(dir - 180)) * length);
+}
+
+// From https://stackoverflow.com/questions/1903954/is-there-a-standard-sign-function-signum-sgn-in-c-c
+template <typename T> int sign(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
+class TestGrid : public rNode {
+public:
+    int slices = 16;
+    int spacing = 1;
+
+    void draw() {
+        DrawGrid(slices, spacing);
+    }
+
+    TestGrid(int _slices = 16, int _spacing = 1) {
+        slices = _slices;
+        spacing = _spacing;
+    }
+};
+
+class FreeCam : public rNode {
+public:
+    Camera3D camera = { { 0.0f, 10.0f, 0.0f }, { 0.0f, 10.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 92.0f, CAMERA_PERSPECTIVE };
+
+    Vector2 rotation;
+
+    float mouseSensitivity = .8;
+
+    float moveSpeed = 16;
+
+    void step() {
+        float delta = GetFrameTime();
+
+        if (IsKeyDown(KEY_W)) {
+            camera.position.x += lengthdir_x(moveSpeed * delta, rotation.x);
+            camera.position.z += lengthdir_z(moveSpeed * delta, rotation.x);
+        }
+
+        if (IsKeyDown(KEY_A)) {
+            camera.position.x += lengthdir_x(moveSpeed * delta, rotation.x - 90);
+            camera.position.z += lengthdir_z(moveSpeed * delta, rotation.x - 90);
+        }
+
+        if (IsKeyDown(KEY_S)) {
+            camera.position.x += lengthdir_x(moveSpeed * delta, rotation.x + 180);
+            camera.position.z += lengthdir_z(moveSpeed * delta, rotation.x + 180);
+        }
+
+        if (IsKeyDown(KEY_D)) {
+            camera.position.x += lengthdir_x(moveSpeed * delta, rotation.x + 90);
+            camera.position.z += lengthdir_z(moveSpeed * delta, rotation.x + 90);
+        }
+
+        if (IsKeyDown(KEY_Q)) {
+            camera.position.y -= moveSpeed * delta;
+        }
+
+        if (IsKeyDown(KEY_E)) {
+            camera.position.y += moveSpeed * delta;
+        }
+        if (IsWindowFocused()) {
+            updateRotation();
+        }
+
+        //std::cout << camera.position.x << ", " << camera.position.y << ", " << camera.position.z << std::endl;
+    }
+
+    void draw() {
+        BeginMode3D(camera);
+    }
+private:
+    void updateRotation() {
+        int xMove = GetMouseX() - GetScreenWidth() / 2;
+        int yMove = GetMouseY() - GetScreenHeight() / 2;
+
+        //std::cout << "x: " << xMove << " y: " << yMove << std::endl;
+
+        rotation.x += xMove * mouseSensitivity;
+        rotation.y -= yMove * mouseSensitivity;
+
+        //rotation.y = Clamp(rotation.y, -89.99999, 89.99999);
+
+        camera.target = Vector3{ camera.position.x + lengthdir_x(1, rotation.x), camera.position.y + lengthdir_y(1, rotation.y), camera.position.z + lengthdir_z(1, rotation.x) };
+
+        SetMousePosition(GetScreenWidth() / 2, GetScreenHeight() / 2);
+    }
+};
+
+class rEnd3D : public rNode {
+    public:
+        void draw() {
+            EndMode3D();
+        }
+};
+
+class rModelRenderer : public rNode {
+    Model model = { 0 };
+    Vector3 position = { 0 };
+    Vector3 rotationAxis = { 0 };
+    float rotationAngle = 0;
+    Vector3 scale = { 1, 1, 1 };
+    Color tint = WHITE;
+    
+    void draw() {
+        if (IsModelValid(model)) DrawModelEx(model, position, rotationAxis, rotationAngle, scale, tint);
+    }
+
+    ~rModelRenderer() {
+        if (IsModelValid(model)) UnloadModel(model);
+    }
+};
+
+class rTimer : public rNode {
+public:
+    void init(float _seconds, bool startActive = false, bool _looping = false) {
+        seconds = _seconds;
+
+        if (startActive) {
+            reset();
+        }
+
+        looping = _looping;
+    }
+
+    void setLength(float _seconds) {
+        seconds = _seconds;
+    }
+
+    float getLength() {
+        return seconds;
+    }
+
+    float setTimeLeft(float _seconds) {
+        asleep = false;
+        finished = false;
+        timeLeft = _seconds;
+    }
+
+    float getTimeLeft() {
+        return timeLeft;
+    }
+
+    float getPercentage() {
+        return timeLeft / seconds;
+    }
+
+    bool isJustFinished() {
+        return justFinished;
+    }
+
+    bool isFinished() {
+        return finished;
+    }
+
+    void setLooping(bool _looping) {
+        looping = _looping;
+    }
+
+    bool getLooping() {
+        return looping;
+    }
+
+    void reset() {
+        timeLeft = seconds;
+        asleep = false;
+        finished = false;
+
+        if (onReset) {
+            onReset();
+        }
+    }
+
+    void step() {
+        if (justFinished) {
+            justFinished = false;
+
+            if (looping) reset();
+        }
+
+        if (!asleep) {
+            timeLeft -= rGetDelta();
+
+            if (timeLeft <= 0) {
+                asleep = true;
+
+                justFinished = true;
+                finished = true;
+
+                if (onFinished) {
+                    onFinished();
+                }
+            }
+        }
+    }
+
+    std::function<void()> onFinished;
+    std::function<void()> onReset;
+
+private:
+    float timeLeft = 0;
+    float seconds = 0;
+    bool asleep = false;
+    bool looping = false;
+
+    bool justFinished = false;
+    bool finished = false;
+};
+
+/*class rLineRenderer : public rNodeBase {
+    public:
+	    rColor color;
+	
+	    void draw() {
+		    int pointsSize = points.size();
+		    if (pointsSize > 1) {
+			    for (int i = 1; i < pointsSize; i++) {
+				    rDrawLine(points[i-1].position, points[i].position, color);
+			    }
+		    }
+	    }
+
+        void addPoint(rVector2 position, rColor color) {
+            points.push_back(Point{ position, color });
+        }
+
+        void insertPoint(int index, rVector2 position, rColor color) {
+            points.insert(index, Point{ position, color });
+        }
+
+        void removePoint(int index) {
+            points.erase(index);
+        }
+
+        void setPointPosition(int index, rVector2 position) {
+            points[index].position = position;
+        }
+
+        rVector2 getPointPosition(int index) {
+            return points[index].position;
+        }
+
+        void setPointColor(int index, rColor color) {
+            points[index].color = color;
+        }
+
+        rColor getPointColor(int index) {
+            return points[index].color;
+        }
+    private:
+        typedef struct Point {
+            rVector2 position,
+            rColor color
+        } Point;
+
+        std::vector<Point> points;
+};*/
+
+template <typename T> class rGrid {
+    public:
+        void setSize(rVector2 _size) {
+            data.clear();
+            data.resize(_size.x * _size.y);
+            
+            size = _size;
+        }
+
+        rVector2 getSize() {
+            return size;
+        }
+
+        T at(rVector2 position) {
+            int index = getIndex(position);
+
+            return data[index];
+        }
+
+        T set(rVector2 position, T value) {
+            int index = getIndex(position);
+
+            data[index] = value;
+        }
+
+    private:
+        std::vector<T> data;
+
+        rVector2 size = { 0, 0 };
+
+        rVector2 getIndex(rVector2 position) {
+            return (position.y * size.y) + position.x;
+        }
+};
