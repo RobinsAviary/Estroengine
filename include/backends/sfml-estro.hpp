13831c9d710b3e645c00ba4d7b983a1469739b6a
RobinsAviary <robinsaviary@gmail.com>
2025-03-21 15:26:47 -0400

    Implemented graphics functions for sfml backend

--- include/backends/sfml-estro.hpp
+++ include/backends/sfml-estro.hpp
@@ -84,20 +84,56 @@ void rDrawTextureReproject(rTexture* texture, rRectangle source, rRectangle targ
 }
 
 void rDrawLine(rVector2 pos1, rVector2 pos2, rColor color) {
-	
+	sf::Color _color = rColorTosfColor(color);
+
+	std::array line =
+	{
+		sf::Vertex{sf::Vector2f(pos1.x, pos1.y), _color},
+		sf::Vertex{sf::Vector2f(pos2.x, pos2.y), _color}
+	};
+
+	window.draw(line.data(), line.size(), sf::PrimitiveType::Lines);
 }
 
 void rDrawRectangle(rRectangle rectangle, rColor color, bool filled) {
-	
+	sf::Color _color = rColorTosfColor(color);
+
+	sf::RectangleShape shape({ static_cast<float>(rectangle.w), static_cast<float>(rectangle.h) });
+	shape.setPosition(sf::Vector2f{ static_cast<float>(rectangle.x), static_cast<float>(rectangle.y) });
+	if (filled) {
+		shape.setFillColor(_color);
+	}
+	else {
+		shape.setOutlineColor(_color);
+		// TODO: Make this able to be changed.
+		shape.setOutlineThickness(1);
+	}
+	window.draw(shape);
 }
 
 void rDrawCircle(rVector2 position, float radius, rColor color, bool filled) {
-	
+	sf::CircleShape shape(radius);
+
+	shape.setOrigin(sf::Vector2f{ .5, .5 });
+	shape.setPosition(sf::Vector2f{ static_cast<float>(position.x), static_cast<float>(position.y) });
+
+	sf::Color _color = rColorTosfColor(color);
+
+	if (filled) {
+		shape.setFillColor(_color);
+	}
+	else {
+		shape.setFillColor(_color);
+		// TODO: Make this able to be changed.
+		shape.setOutlineThickness(1);
+	}
+
+	window.draw(shape);
 }
 
 // NOTE: DO NOT use in large amounts, this is generally not an efficient way to do whatever you're doing. Look into editing the textures directly or simplifying shapes.
 void rDrawPixel(rVector2 position, rColor color) {
-	
+	rDrawRectangle(rRectangle{ position.x, position.y, 1, 1 }, color, true);
 }
 
 void rBeginStep() {
