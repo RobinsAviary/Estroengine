e930329674af6c394d1240744fcf040950b78863
RobinsAviary <robinsaviary@gmail.com>
2025-04-01 23:57:45 -0400

    Base estropath impl.

--- include/estropathing.hpp
+++ include/estropathing.hpp
@@ -3,5 +3,34 @@
 
 class rPathingGrid : public rNode {
 public:
+	AStar::AStar<> pathfinder;
 
+	void setSize(rVector2<unsigned int> _size) {
+		pathfinder.setWorldSize({ static_cast<int>(_size.x), static_cast<int>(_size.y) });
+	}
+
+	void setDiagonalMovement(bool enable) {
+		pathfinder.setDiagonalMovement(enable);
+	}
+
+	void set(rVector2<unsigned int> position, bool block) {
+		if (block) {
+			pathfinder.addObstacle({ static_cast<int>(position.x), static_cast<int>(position.y) });
+		}
+		else {
+			pathfinder.removeObstacle({ static_cast<int>(position.x), static_cast<int>(position.y) });
+		}
+	}
+
+	rList<rVector2<unsigned int>> getPath(rVector2<unsigned int> startPos, rVector2<unsigned int> endPos) {
+		rList<rVector2<unsigned int>> result;
+
+		auto path = pathfinder.findPath({ static_cast<int>(startPos.x), static_cast<int>(startPos.y) }, {static_cast<int>(endPos.x), static_cast<int>(endPos.y)});
+
+		for (auto& point : path) {
+			result.add(rVector2<unsigned int>{static_cast<unsigned int>(point.x), static_cast<unsigned int>(point.y)});
+		}
+
+		return result;
+	}
 };
