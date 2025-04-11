e10faed6532f4b8eeb5b41a59efacfa23622c4ce
RobinsAviary <robinsaviary@gmail.com>
2025-04-10 21:43:33 -0400

    cleaned up old system, reimpl tags fully

--- estroengine.hpp
+++ estroengine.hpp
@@ -117,111 +117,101 @@ class rNode {
 			return static_cast<T*>(children[index]);
 		}
 
-		// Old query system
-		template <typename T = rNode>
-		T* getAncestorTagged(std::string tag) {
+		rNode* getAncestorTagged(std::string tag) {
 			auto ancestors = getAncestors();
 
 			for (auto ancestor : ancestors) {
 				if (ancestor->isTagged(tag)) {
-					return static_cast<T*>(ancestor);
+					return static_cast<rNode*>(ancestor);
 				}
 			}
 
 			return NULL;
 		}
 
-		template <typename T = rNode>
-		rList<T*> getAncestorsTagged(std::string tag) {
+		rList<rNode*> getAncestorsTagged(std::string tag) {
 			auto ancestors = getAncestors();
 
 			rList<rNode*> result;
 
 			for (auto ancestor : ancestors) {
 				if (ancestor->isTagged(tag)) {
-					result.add(static_cast<T>(ancestor));
+					result.add(static_cast<rNode*>(ancestor));
 				}
 			}
 
 			return result;
 		}
 
-		template <typename T = rNode>
-		T* getChildTagged(std::string tag) {
+		rNode* getChildTagged(std::string tag) {
 			for (auto child : children) {
-				if (child->isTagged(tag)) return static_cast<T>(child);
+				if (child->isTagged(tag)) return static_cast<rNode*>(child);
 			}
 
 			return NULL;
 		}
 
-		template <typename T = rNode>
-		T* getDescendantTagged(std::string tag) {
+		rNode* getDescendantTagged(std::string tag) {
 			auto descendants = getDescendants();
 
 			for (auto descendant : descendants) {
-				if (descendant->isTagged(tag)) return static_cast<T>(descendant);
+				if (descendant->isTagged(tag)) return static_cast<rNode*>(descendant);
 			}
 
 			return NULL;
 		}
 
-		template <typename T = rNode>
-		rList<T*> getDescendantsTagged(std::string tag) {
+		rList<rNode*> getDescendantsTagged(std::string tag) {
 			auto _children = getDescendants();
 			rList<rNode*> result;
 
 			for (auto child : _children) {
 				if (child->isTagged(tag)) {
-					result.add(static_cast<T>(child));
+					result.add(static_cast<rNode*>(child));
 				}
 			}
 
 			return result;
 		}
 
-		template <typename T = rNode>
-		rList<T*> getSiblingsTagged(std::string tag) {
-			rList<T*> result;
+		rList<rNode*> getSiblingsTagged(std::string tag) {
+			rList<rNode*> result;
 
 			rList<rNode*> siblings = getSiblings();
 
 			for (auto sibling : siblings) {
-				if (sibling->hasTag(tag)) {
-					result.add(static_cast<T*>(sibling));
+				if (sibling->isTagged(tag)) {
+					result.add(static_cast<rNode*>(sibling));
 				}
 			}
 
 			return result;
 		}
 
-		template <typename T = rNode>
-		T* getSiblingTagged(std::string tag) {
+		rNode* getSiblingTagged(std::string tag) {
 			rList<rNode*> siblings = getSiblings();
 
 			for (auto sibling : siblings) {
-				if (sibling->hasTag(tag)) {
-					return static_cast<T*>(sibling);
+				if (sibling->isTagged(tag)) {
+					return static_cast<rNode*>(sibling);
 				}
 			}
 
 			return NULL;
 		}
 
-		template <typename T = rNode>
-		rList<T*> getChildrenTagged(std::string tag) {
-			rList<T*> result;
+		rList<rNode*> getChildrenTagged(std::string tag) {
+			rList<rNode*> result;
 
 			for (auto child : children) {
-				if (child->hasTag(tag)) {
-					result.add(static_cast<T*>(child));
+				if (child->isTagged(tag)) {
+					result.add(static_cast<rNode*>(child));
 				}
 			}
 
 			return result;
 		}
 
-		// New Query system
 		rList<rNode*> getAncestors() {
 			rList<rNode*> result;
 
