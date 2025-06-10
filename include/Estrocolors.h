be7d3cd742b25d22e5d63d4bc3c06d58205f0157
RobinsAviary <robinsaviary@gmail.com>
2025-06-10 10:14:46 -0400

    docs

--- include/Estrocolors.h
+++ include/Estrocolors.h
@@ -2,9 +2,13 @@
 
 namespace Estro {
 	struct Color {
+		//!Red value.
 		unsigned char r = 0;
+		//!Green value.
 		unsigned char g = 0;
+		//!Blue value.
 		unsigned char b = 0;
+		//!Alpha value.
 		unsigned char a = 255;
 
 		Color(unsigned char red = 0, unsigned char green = 0, unsigned char blue = 0, unsigned char alpha = 255);
@@ -22,13 +26,23 @@ namespace Estro {
 		bool operator==(const Color& rhs);
 		bool operator!=(const Color& rhs);
 
+		//!Sets the Color using Red, Green, and Blue. Assumes Alpha to be 255.
 		void rgb(unsigned char red = 0, unsigned char green = 0, unsigned char blue = 0);
-		void rgba(unsigned char red = 0, unsigned char green = 0, unsigned char blue = 0, unsigned char alpha = 0);
+		//!Sets the Color using Red, Green, Blue, and Alpha.
+		void rgba(unsigned char red = 0, unsigned char green = 0, unsigned char blue = 0, unsigned char alpha = 255);
+		//!Sets the Color using Hue, Saturation, and Value. Assumes alpha to be 255.
 		void hsv(unsigned int hue = 0, unsigned char saturation = 0, unsigned char value = 0);
-		void hsva(unsigned int hue = 0, unsigned char value = 0, unsigned char saturation = 0, unsigned char alpha = 0);
+		//!Sets the Color using Hue, Saturation, Value, and Alpha.
+		void hsva(unsigned int hue = 0, unsigned char value = 0, unsigned char saturation = 0, unsigned char alpha = 255);
+
+		/*!Averages the base color with the supplied color.
+		@param color The color to mix in.
+		*/
+
 		void mix(Color color);
 	};
 
+	//!Estroengine's default colors.
 	namespace Colors {
 		inline Color White = { 255, 255, 255, 255 };
 		inline Color Black = { 0, 0, 0, 255 };
@@ -43,6 +57,7 @@ namespace Estro {
 		inline Color Brown = { 84, 45, 19, 255 };
 		inline Color Purple = { 140, 0, 255, 255 };
 		inline Color HotPink = { 255, 0, 110, 255 };
+		//!A basic light blue color.
 		inline Color Estrogen = { 142, 236, 255, 255 };
 	}
 }
--- src/Estrovector.cpp
+++ src/Estrovector.cpp
@@ -61,6 +61,7 @@ bool Vector2<T>::operator!=(const Vector2<T>& rhs) {
 	return (x != rhs.x || y != rhs.y);
 }
 
+//!Returns a swizzled vector of y and x.
 template <typename T>
 Vector2<T> Vector2<T>::yx() {
 	return Vector2<T>{y, x};
@@ -74,36 +75,43 @@ Rectangle<T>::Rectangle(T xPosition, T yPosition, T width, T height) {
 	h = height;
 }
 
+//!Returns a swizzled vector of y, x, and z.
 template <typename T>
 Vector3<T> Vector3<T>::yxz() {
 	return Vector3<T>{y, x, z};
 }
 
+//!Returns a swizzled vector of z, x, and y.
 template <typename T>
 Vector3<T> Vector3<T>::zxy() {
 	return Vector3<T>{z, x, y};
 }
 
+//!Returns a swizzled vector of x, z, and y.
 template <typename T>
 Vector3<T> Vector3<T>::xzy() {
 	return Vector3<T>{x, z, y};
 }
 
+//!Returns a swizzled vector of y, z, and x.
 template <typename T>
 Vector3<T> Vector3<T>::yzx() {
 	return Vector3<T>{y, z, x};
 }
 
+//!Returns a swizzled vector of z, y, and x.
 template <typename T>
 Vector3<T> Vector3<T>::zyx() {
 	return Vector3<T>{z, y, x};
 }
 
+//!Returns a Vector2 of the x and y values.
 template <typename T>
 Vector2<T> Vector3<T>::xy() {
 	return Vector2<T>{x, y};
 }
 
+//!Returns a Vector2 of the y and x values.
 template <typename T>
 Vector2<T> Vector3<T>::yx() {
 	return Vector2<T>{y, x};
